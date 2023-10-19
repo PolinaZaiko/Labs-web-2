aef9f403536ec65f76f0c0f76d02ebdce09b113d
ZaikoPolina <zpolina2003@mail.ru>
2023-10-19 21:55:52 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -29,3 +29,40 @@ def login():
     error = 'Неверный логин и/или пароль'
     return render_template('login.html', error=error, username=username, password=password)
 
+@lab4.route('/lab4/holod', methods=['GET', 'POST'])
+def holod():
+    if request.method == 'GET':
+        return render_template('holod.html')
+
+    temperature = request.form.get('temperature')
+
+    if not temperature:
+        error = 'ошибка: не задана температура'
+        return render_template('holod.html', error=error, temperature=temperature, snowflakes=0)
+    
+    temperature = int(temperature)  # Преобразование в целое число
+
+    snowflakes = 0
+
+    if temperature < -12:
+        error = "Не удалось установить температуру — слишком низкое значение"
+        return render_template('holod.html', error=error, temperature=temperature, snowflakes=snowflakes)
+
+    elif temperature >= -12 and temperature <= -9:
+        error = "Установлена температура: {}°С".format(temperature)
+        snowflakes = 3
+        return render_template('holod.html', error=error, temperature=temperature, snowflakes=snowflakes)
+
+    elif temperature >= -8 and temperature <= -5:
+        error = "Установлена температура: {}°С".format(temperature)
+        snowflakes = 2
+        return render_template('holod.html', error=error, temperature=temperature, snowflakes=snowflakes)
+
+    elif temperature >= -4 and temperature <= -1:
+        error = "Установлена температура: {}°С".format(temperature)
+        snowflakes = 1
+        return render_template('holod.html', error=error, temperature=temperature, snowflakes=snowflakes)
+
+    else:
+        error = "Не удалось установить температуру — слишком высокое значение"
+        return render_template('holod.html', temperature=temperature, error=error, snowflakes=snowflakes)
