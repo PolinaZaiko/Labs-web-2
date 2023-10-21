17de172e414c9f32e54f59c385964ba731e24543
ZaikoPolina <zpolina2003@mail.ru>
2023-10-21 22:23:35 +0700

    Куки — цвет текста

--- lab4.py
+++ lab4.py
@@ -108,5 +108,26 @@ def zerno():
     return render_template('zerno.html', zerno=zerno, weight=weight, total_price=total_price)
 
 
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET': 
+        return render_template('cookies.html')
+    
+    color = request.form.get('color')
+    bg_color = request.form.get('bg_color')
+    font_size = request.form.get('font_size')
 
+    if color == bg_color:
+        error = 'Цвет текста не должен совпадать с цветом фона'
+        return render_template('cookies.html', error=error)
+    
+    headers = {
+        'Set-Cookie': [
+            'color=' + color + ';path=/',
+            'bg_color=' + bg_color + ';path=/',
+            'font_size=' + font_size + ';path=/'
+            ],
+        'Location': '/lab4/cookies'
+}
 
+    return '', 303, headers
