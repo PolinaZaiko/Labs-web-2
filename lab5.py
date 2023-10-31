127cc6642ccaca444eaae02d7590fb312a7459fe
ZaikoPolina <zpolina2003@mail.ru>
2023-10-31 13:36:24 +0700

    Внесла изменения + листинг

--- lab5.py
+++ lab5.py
@@ -3,15 +3,24 @@ import psycopg2
 
 lab5 = Blueprint("lab5", __name__)
 
-@lab5.route ("/lab5")
-def main():
-# Прописываем параметры подключения к БД
+def dbConnect():
     conn = psycopg2.connect (
         host="127.0.0.1", 
         database="Knowledge_base_for_Zaiko_Polina",
         user="Zaiko_Polina_knowledge_base", 
         password="12345")
+    
+    return conn;
 
+def dbClose (cursor, connection):
+# Закрываем курсор и соединение
+# Порядок важен!
+    cursor.close ()
+    connection.close ()
+
+@lab5.route ("/lab5")
+def main():
+    conn = dbConnect()
     # Получаем курсор. С помощью него мы можем выполнять SQL-запросы
     cur = conn.cursor ()
 
@@ -20,39 +29,23 @@ def main():
 
     # fetchall - получить все строки, которые получились в результате # выполнения SQL-запроса в execute
     # Сохраняем эти строки в переменную result
-    result = cur.fetchall ()
-
-    # Закрываем соединение с БД
-    cur.close ()
-    conn.close ()
+    result = cur.fetchall()
 
     print (result)
+
+    dbClose(cur, conn)
 
     return "go to console"
 
 
 @lab5.route ("/lab5/users")
 def users():
-# Прописываем параметры подключения к БД
-    conn = psycopg2.connect (
-        host="127.0.0.1", 
-        database="Knowledge_base_for_Zaiko_Polina",
-        user="Zaiko_Polina_knowledge_base", 
-        password="12345")
-
-    # Получаем курсор. С помощью него мы можем выполнять SQL-запросы
+    conn = dbConnect()
     cur = conn.cursor ()
 
-    # Пишем запрос, который psycog2 должен выполнить
     cur.execute ("SELECT * FROM users;")
 
-    # fetchall - получить все строки, которые получились в результате # выполнения SQL-запроса в execute
-    # Сохраняем эти строки в переменную result
-    result = cur.fetchall ()
-
-    # Закрываем соединение с БД
-    cur.close ()
-    conn.close ()
+    result = cur.fetchall()
 
     # Создаем пустой список для хранения имен пользователей
     names = []
@@ -60,5 +53,7 @@ def users():
      # Проходимся по каждой строке в результате и добавляем имена в список
     for user in result:
         names.append(user[1])
+
+    dbClose(cur, conn)     
 
     return render_template('users.html', names=names)
