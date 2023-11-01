23a0a6237a899a59943492d3e1404d582b485af0
ZaikoPolina <zpolina2003@mail.ru>
2023-11-01 18:45:32 +0700

    Роут для новых статей

--- lab5.py
+++ lab5.py
@@ -120,3 +120,36 @@ def loginPage():
     else:
         errors.append ("Неправильный логин или пароль")
         return render_template ("log.html", errors=errors)
+    
+
+@lab5.route ("/lab5/new_article", methods=["GET", "POST"]) 
+def createArticle():
+    errors = []
+
+    userID = session.get("id")
+
+    if userID is not None:
+        if request.method == "GET":
+            return render_template("new_article.html")
+        
+        if request.method == "POST":
+            text_article = request.form.get("text_article")
+            title = request.form.get("title_article")
+            
+            if len(text_article) == 0:
+                errors.append ("Заполните текст")
+                return render_template ("new_article.html", errors=errors)
+            
+            conn = dbConnect()
+            cur = conn.cursor()
+            
+            cur.execute (f"INSERT INTO articles (user_id, title, article_text) VALUES ({userID}, '{title}', '{text_article}') RETURNING id")
+
+            new_article_id = cur.fetchone()[0]
+            conn.commit ()
+
+            dbClose (cur,conn)
+    
+            return redirect (f"/lab5/articles/{new_article_id}")
+            
+        return redirect(")/lab5/login")
