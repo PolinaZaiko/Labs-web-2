9c2a2ac82aa467c8cda342423b7ec40679704a65
ZaikoPolina <zpolina2003@mail.ru>
2023-11-15 16:57:36 +0700

    Подключение ORM к проекту

--- app.py
+++ app.py
@@ -6,11 +6,30 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 
+from flask_sqlalchemy import SQLAlchemy  
+from Db import db 
+from Db.models import users 
+from flask_login import LoginManager
+
+
 app=Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
-app.secret_key = '123'
 app.register_blueprint(lab6)
+
+app.secret_key = "12345"
+user_db = "Zaiko_Polina_knowledge_base_orm"
+host_ip = "127.0.0.1"
+host_port = "5432"
+database_name = "Knowledge_base_orm"
+password = "12345"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
