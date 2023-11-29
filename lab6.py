70724ab85f7a13190a7e9000007795f636136486
ZaikoPolina <zpolina2003@mail.ru>
2023-11-29 16:46:50 +0700

    Заготовка к 7-ой лабораторной

--- lab6.py
+++ lab6.py
@@ -4,7 +4,6 @@ from Db import db
 from Db.models import users, articles
 from werkzeug. security import check_password_hash, generate_password_hash
 from flask_login import login_user, login_required, current_user, logout_user
-import psycopg2
 
 
 lab6 = Blueprint ("lab6", __name__)
