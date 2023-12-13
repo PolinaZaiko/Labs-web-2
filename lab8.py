f5fbca558e55e29ad88cfb5cf561972d103e371b
ZaikoPolina <zpolina2003@mail.ru>
2023-12-13 14:36:45 +0700

    Добавление и Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -5,3 +5,26 @@ lab8 = Blueprint ("lab8", __name__)
 @lab8.route('/lab8/index')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "C++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "C#", "videos": 8}
+]
+
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return courses
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        return "Error 404: Course not found", 404
+    return courses[course_num]
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        return "Error 404: Course not found", 404
+    del courses[course_num]
+    return '', 204
