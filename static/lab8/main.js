2b2804026d5c0987c099bbd0cfb7696b1544ba31
ZaikoPolina <zpolina2003@mail.ru>
2023-12-13 17:05:34 +0700

    Вывод таблицы

---
+++ static/lab8/main.js
@@ -0,0 +1,50 @@
+function fillCourseList() {
+    fetch('/lab8/api/courses/')
+
+    .then(function(data) {
+        return data.json();
+    })
+    .then(function(courses){
+        let tbody = document.getElementById('course-list');
+        tbody.innerHTML = '';
+        for (let i = 0; i<courses.length; i++) {
+            tr = document.createElement('tr');
+
+            let tdName = document.createElement('td');
+            tdName.innerText = courses[i].name;
+            
+            let tdVideos = document.createElement('td');
+            tdVideos.innerText = courses[i].videos;
+
+            let tdPrice = document.createElement('td');
+            tdPrice.innerText = courses[i].price || 'бесплатно';
+
+            let tdDate = document.createElement('td');
+            tdDate.innerText = courses[i].date_;
+
+            let editBut = document.createElement('button');
+            editBut.innerText = 'редактировать';
+            editBut.onclick = function() {
+                editCourse(i, courses[i]);
+            }
+
+            let delBut = document.createElement('button');
+            delBut.innerText = 'удалить';
+            delBut.onclick = function(){
+                deleteCourse(i);
+            }
+
+            let tdActions = document.createElement('td');
+            tdActions.append(editBut);
+            tdActions.append(delBut);
+
+            tr.append(tdName);
+            tr.append(tdVideos);
+            tr.append(tdPrice);
+            tr.append(tdDate);
+            tr.append(tdActions);
+            
+            tbody.append(tr);
+        }
+    })
+}
