21a97f0703392717a0f5c09ef281dff84fa9254a
ZaikoPolina <zpolina2003@mail.ru>
2023-12-19 11:39:16 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -8,5 +8,13 @@ def main ():
 
 @lab9.app_errorhandler(404)
 def not_found(e):
-    return render_template('lab9/error.html'), 404
+    return render_template('lab9/error404.html'), 404
 
+@lab9.app_errorhandler(500)
+def server_error(e):
+    return render_template('lab9/error500.html'), 500
+
+# Роут для вызова ошибки 500
+@lab9.route('/lab9/cause500')
+def cause_error():
+    return 1 / 0
