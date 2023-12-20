871e047eda32ee5e748339cf5de165bd5809611f
ZaikoPolina <zpolina2003@mail.ru>
2023-12-20 10:41:42 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 lab9 = Blueprint ("lab9", __name__)
 
@@ -18,3 +18,20 @@ def server_error(e):
 @lab9.route('/lab9/cause500')
 def cause_error():
     return 1 / 0
+
+
+@lab9.route('/lab9/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        recipient_name = request.form.get('recipient_name')
+        recipient_gender = request.form.get('recipient_gender')
+        sender_name = request.form.get('sender_name')
+        return redirect(url_for('lab9.greeting_card', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name))
+    return render_template('lab9/index.html')
+
+@lab9.route('/lab9/greeting_card/')
+def greeting_card():
+    recipient_name = request.args.get('recipient_name')
+    recipient_gender = request.args.get('recipient_gender')
+    sender_name = request.args.get('sender_name')
+    return render_template('lab9/greeting_card.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
